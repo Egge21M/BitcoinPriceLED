75ae0874fb4babf5136c3963001ec313ef8cbb44
Egge <egge24@protonmail.com>
2021-08-02 17:57:45 +0200

    Added neopixel LED functionality

--- src/main.py
+++ src/main.py
@@ -1,14 +1,34 @@
 from prices import getPrices
 import time
 from datetime import datetime
+from neopixel import *
 
 import logging
 
 logging.basicConfig(filename='error.log')
 
+errorCount = 0
+
+# LED Strip Config
+LED_COUNT = 32
+LED_PIN = 18
+LED_FREQ_HZ = 800000
+LED_DMA = 5
+LED_BRIGHTNESS = 100
+LED_INVERT = False
+
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+
+
+
 def checkTrend(oldPrice, currentPrice):
-    trend = float(currentPrice) / float(oldPrice)
-    return trend
+    quotient = float(currentPrice) / float(oldPrice)
+    if quotient >= 1: 
+        trend = quotient - 1
+        return 1, trend
+    elif quotient < 1:
+        trend = 1 - quotient
+        return 0, trend
 
 def priceUpdater(currentPrice):
     oldPrice = currentPrice
@@ -18,22 +38,38 @@ def priceUpdater(currentPrice):
 
 def run():
     currentPrice = getPrices('BTCUSD')
+    errorCount = 0
     while True:
         time.sleep(300)
         try:
+            strip.begin()
             prices = priceUpdater(currentPrice)
             oldPrice = prices[1]
             currentPrice = prices[0]
             trend = checkTrend(oldPrice, currentPrice)
             now = datetime.now()
-            if trend > 1:
-                print(f'Uhrzeit: {now.strftime("%H:%M:%S")} - Alter Preis: {oldPrice}, aktueller Preis: {currentPrice}, Trend: +{trend-1}%')
-            elif trend < 1:
-                print(f'Uhrzeit: {now.strftime("%H:%M:%S")} - Alter Preis: {oldPrice}, aktueller Preis: {currentPrice}, Trend: -{1-trend}%')
+            if errorCount > 3:
+                logging.error(f'ERROR-Mode activated - Currently {errorCount} failures in a row')
+                color = (247,147,26)
+                errorCount = 0
+
+            if trend[0] == 1:
+                print(f'Uhrzeit: {now.strftime("%H:%M:%S")} - Alter Preis: {oldPrice}, aktueller Preis: {currentPrice}, Trend: +{trend[1]}%')
+                color = (0, 255, 0)
+                errorCount= 0
+
+            elif trend == 0:
+                print(f'Uhrzeit: {now.strftime("%H:%M:%S")} - Alter Preis: {oldPrice}, aktueller Preis: {currentPrice}, Trend: -{trend[1]}%')
+                color = (255, 0, 0)
+                errorCount = 0
         
         except Exception as e:
-            logging.error(f'Error while checking prices and trend')
-            print(f'Error: Preistrend konnte nicht abgerufen werden')
+            errorCount += 1
+        for i in range(0, strip.numPixels()):
+            strip.setPixelColor(i, Color(color[0],color[1],color[2]))
+            strip.show()
+            time.sleep(0.1)
+
 
 run()
 
--- src/prices.py
+++ src/prices.py
@@ -1,9 +1,16 @@
 import requests
 import json
+import logging
+
 
 TICKER_URL = 'https://www.bitstamp.net/api/v2/ticker/'
 
 def getPrices(pair):
-    r = requests.get(TICKER_URL+pair).content
-    dict = json.loads(r)
-    return dict['last']
+    try:
+        r = requests.get(TICKER_URL+pair).content
+        dict = json.loads(r)
+        return dict['last']
+    except Exception as e:
+        logging.error(f'Error while getting current prices')
+        return None
+
