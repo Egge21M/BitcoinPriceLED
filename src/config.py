650fd5e9a13816d0bd061ee5c95318be5a09d3eb
Egge <80262989+Egge7@users.noreply.github.com>
2021-09-13 14:35:26 +0200

    Initial Commit for Web-remote

--- src/config.py
+++ src/config.py
@@ -1,27 +1,10 @@
-# ------- EDIT -------
-
-# Testmode
-# If testing is set to True prices will get updated every 30 seconds
-testing = False
-
-# Interval
-# Sets the time (in seconds) that the script will wait before getting a net price and updating the trend (default: 900)
-# Warning: Small intervals (e.g. 1-10) will result in alot of requests and might get your IP address banned by the API provider.
-interval = 900
-
-# Static
-# If static is set to True LEDs won't update to represent the Bitcoin price trend, they will have the color defined in staticColor 
-static = False
-staticColor = (0,0,0)
-
-# Nightmode
-# If nightmode is set to True LEDs will be dimmed at certrain times (beginnSleep - stopSleep).
-nightmode = True
-beginSleep = 22
-stopSleep = 7
-
-
-# ------- DON'T EDIT -------
+testing=False
+interval=555
+static=True
+staticColor =(255, 255, 255)
+nightmode=True
+beginSleep=21
+stopSleep=4
 
 
 # LED Strip Config
